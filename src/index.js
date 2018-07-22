e79a25e2327622347027accd5bdd0f65414a86f2
Tony Brown <tony@tonybrown.me>
2018-07-22 06:01:17 -0400

    refactor code to run more efficiently

--- src/index.js
+++ src/index.js
@@ -1,20 +1,52 @@
 import h from 'hyperscript'
 import hh from 'hyperscript-helpers'
 
-const { div, button  } = hh(h)
-const initModel = 0
-
+const { div, button } = hh(h)
+// model
+const initModel = 0;
 // view function
-function view(model) {
+function view(dispatch, model) {
   return div([
     div({ className: 'mv2' },`Count: ${model}`),
-    button({ className: 'pv1 ph2 mr2', 
-      onclick: () => console.log('+ clicked') }, '+'),
-    button({ className: 'pv1 ph2', 
-      onclick: () => console.log('- clicked') }, '-'),
-  ])
+      button({ className: 'pv1 ph2 mr2', 
+        onclick: () => dispatch(MSGS.ADD) }, '+'),
+      button({ className: 'pv1 ph2', 
+        onclick: () => dispatch(MSGS.SUBTRACK) }, '-'),
+  ]);
 }
+
+const MSGS = {
+  ADD: 'ADD',
+  SUBTRACK: 'SUBTRACK',
+}
+
+// update function
+function update(msg, model) {
+  switch (msg) {
+    case MSGS.ADD: 
+      return model + 1
+      break
+    case MSGS.SUBTRACK:
+      return model - 1
+      break
+    default:
+     return model
+  }
+}
+  // impure code below
+  function app(initModel, update, view, node) {
+    let model = initModel;
+    let currentView = view(dispatch, model);
+    node.appendChild(currentView);
+    function dispatch(msg) {
+      model = update(msg, model);
+      const updatedView = view(dispatch, model)
+      node.replaceChild(updatedView, currentView);
+      currentView = updatedView;
+    }
+  }
 
 const rootNode = document.getElementById('app');
 
-rootNode.appendChild(view(initModel))
+app(initModel, update, view, rootNode);
+//  rootNode.appendChild(view(update('plus', initModel)))
